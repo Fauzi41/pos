8ece36ebec12e91c04ee386faff659cf584edfb4
Fauzi41 <anasfauzi1997@gmail.com>
2022-04-26 14:34:54 +0700

    added verify

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from lib2to3.pgen2 import token
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import pymongo
 import uuid
+# from flask_mail import Mail, Message
 # from handler import routes
 
 
@@ -8,27 +10,46 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 client = pymongo.MongoClient('localhost', 27017)
 db = client.flaskapp
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'codionapp@gmail.com'
+app.config['MAIL_PASSWORD'] = 'codion123'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+# mail = Mail(app)
 
 @app.route('/')
 def index():
     return render_template('signup.html')
-
+  
+@app.route('/profile')
+def index():
+    return render_template('profile.html')
 
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     user = {
-      "_id": uuid.uuid4().hex,
+      "id": uuid.uuid4().hex,
       "email": request.form.get('email'),
-      "password": request.form.get('password')
+      "password": request.form.get('password'),
+      "token":uuid.uuid4().hex,
+      "verified":False
     }
     
     if db.shopper.find_one({ "email": user['email'] }):
       return jsonify({ "error": "Email address already in use" }), 400
 
     if db.shopper.insert_one(user):
-      return render_template('signup.html')
+      
+      return redirect(url_for("profile")
 
     return jsonify({ "error": "Signup failed" }), 400
+
+@app.route('/verify/<token>')
+def verify(token):
+  print(token)
+  return redirect(url_for("profile"))
 
 if __name__ == "__main__":
     
